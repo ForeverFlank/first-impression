2e59b459e3b5b5d4ad93a7e1e0ce0f78a12112b3
JunoUno507 <kspanim8@gmail.com>
2023-06-25 20:09:46 +0700

    newsticker optimization

--- newsticker.js
+++ newsticker.js
@@ -1,8 +1,8 @@
 var news = [
-    'ทดสอบ 10<sup>10</sup>',
-    'ทดสอบ 10<sup>20</sup>',
-    'ทดสอบ 10<sup>30</sup>',
-    'ทดสอบ 10<sup>40</sup>'
+    'ทดสอบ 10<sup>10</sup> กขคง----------------------------',
+    'ทดสอบ 10<sup>20</sup> กขคง----------------------------',
+    'ทดสอบ 10<sup>30</sup> กขคง----------------------------',
+    'ทดสอบ 10<sup>40</sup> กขคง----------------------------ทดสอบ 10<sup>40</sup> กขคง----------------------------'
 ];
 
 var index = Math.floor(Math.random() * news.length);
@@ -21,36 +21,19 @@ var newstickerBg = document.getElementById('newstickerBg');
 newsticker.innerHTML = ' ';
 var width = newsticker.offsetWidth;
 var pos = newstickerBg.offsetWidth;
+// distance = pos + width + 200
 
-var scroll = (id) => document.getElementById(id).animate(
-    [
-        { transform: `translateX(${pos + width + 100}px)`},
-        { transform: `translateX(${-width - 100}px)`}
-    ],  {
-      duration: 1600 + pos * 10
-    }
-);
-
-setInterval(function() {
-    scroll('newsticker');
-    newsticker.style.left = `${-width - 100}px`;
+function showNews() {
     newsticker.innerHTML = randNews();
     width = newsticker.offsetWidth;
     pos = newstickerBg.offsetWidth;
-}, 1600 + pos * 10 + 1000);
-
-// scroll('newsticker');
-/*
-setInterval(function() {
-    if (pos + width + 400 >= 0) {
-        newsticker.style.left = pos + 'px';
-        pos -= 1;
-    }
-    else {
-        newsticker.innerHTML = randNews();
-        width = newsticker.offsetWidth;
-        pos = newstickerBg.offsetWidth;
-        newsticker.style.left = pos + 'px';
-    }
-}, 10);
-*/
+    newsticker.animate([
+        { transform: `translateX(${pos + 150}px)`},
+        { transform: `translateX(${-width - 150}px)`}
+    ],  {
+            duration: (pos + width + 300) * 10 + 100
+        }
+    );
+    setTimeout(showNews, (pos + width + 300) * 10);
+}
+setTimeout(showNews, 1);    // oversafety
