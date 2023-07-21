1757ecb374a6d34a28f9ad1dbd2eb06b17670d7c
JunoUno507 <kspanim8@gmail.com>
2023-07-21 11:19:54 +0700

    another refactor

---
+++ impression/im-autobuyer.js
@@ -0,0 +1,23 @@
+// TODO: Optimize this
+function imAutobuyer(n) {
+    var button = document.getElementById(`imAb${n+1}`);
+    var toggle = document.getElementById(`imAb${n+1}T`);
+    if (imAutobuy[n]) {
+        if (imAbToggle[n]) {
+            button.style.backgroundColor = '#99b897';   // take a look at css
+            while (BigNum.greater(im, imCost(n))) {
+                imSub(imCost(n));
+                imUpgrade[n] = BigNum.add(imUpgrade[n], bn(1));
+                imTotal[n] = BigNum.add(imTotal[n], bn(1));
+                setText(`imUp${n+1}`, imCost(n).smartToString(0));
+                setText(`imUp${n+1}T`, imTotal[n].smartToString(0, 'gray', 6));
+                setText(`imUp${n+1}R`, imValue(n).smartToString(0, 'gray', 6));
+            }
+        }
+    }
+    else {
+        toggle.disabled = true;
+        toggle.style.backgroundColor = "#ecb2b2";
+        button.disabled = !BigNum.greater(im, imAbCost[n]);
+    }
+}
--- impression/im-click.js
+++ impression/im-click.js
@@ -1,13 +1,15 @@
 window.imClick = (n) => {
-    console.log(n, imUnlocked);
+    
     if (BigNum.greater(im, imCost(n))) {
         imSub(imCost(n));
         imUpgrade[n] = BigNum.add(imUpgrade[n], bn(1));
         imTotal[n] = BigNum.add(imTotal[n], bn(1));
         if (n == imUnlocked) {
             imUnlocked++;
-            document.getElementById(`imUp${imUnlocked + 1}Div`).style.display = 'flex';
-            setText(`imUp${imUnlocked + 1}`, 'Unlock : ' + imCost(imUnlocked).smartToString(0));
+            if (imUnlocked < 5) {
+                document.getElementById(`imUp${imUnlocked + 1}Div`).style.display = 'flex';
+                setText(`imUp${imUnlocked + 1}`, 'Unlock : ' + imCost(imUnlocked).smartToString(0));
+            }
         }
         setText(`imUp${n+1}`, imCost(n).smartToString(0));
     }
